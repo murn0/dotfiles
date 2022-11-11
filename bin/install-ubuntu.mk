b747fda44659ac5611420c1250bdb8b74faae3a8
murn0 <murano@mur.tokyo>
2022-11-12 02:56:27 +0900

    feat(debian): Debianの場合にもFishShellが正常にインストールできるようにインストール機能追加

--- bin/install-ubuntu.mk
+++ bin/install-ubuntu.mk
@@ -2,16 +2,34 @@ include $(PWD)/bin/colors.mk
 
 MAKEFILE_PATH := $(shell dirname $(realpath $(dir $(lastword $(MAKEFILE_LIST)))))
 
+OS_NAME := $(shell grep -e ^ID= /etc/os-release | awk -F'[=]' '{ print $$2 }')
+VERSION_CODENAME := $(shell grep -e ^VERSION_CODENAME= /etc/os-release | awk -F'[=]' '{ print $$2 }')
+
 .PHONY: $(shell cat $(MAKEFILE_LIST) | awk -F':' '/^[a-z0-9_-]+:/ {print $$1}')
 
 all: install-fish install-fisher install-packages
 
 install-fish: ## Install fish shell (https://fishshell.com/)
+ifeq ($(OS_NAME), ubuntu)
 	@echo "${YELLOW}▓▒░ Installing fish shell${RESET}"
 	@sudo apt update
 	@sudo apt-get install -y software-properties-common
 	@sudo apt-add-repository -y ppa:fish-shell/release-3
 	@sudo apt -y install --no-install-recommends fish;
+else ifeq ($(OS_NAME), debian)
+ifeq ($(VERSION_CODENAME), stretch)
+	@echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_9.0/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+	@curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_9.0/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+else ifeq ($(VERSION_CODENAME), buster)
+	@echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+	@curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+else ifeq ($(VERSION_CODENAME), bullseye)
+	@echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+	@curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+endif
+	@sudo apt update
+	@sudo apt -y install --no-install-recommends fish;
+endif
 	@echo "${BLUE}${BOLD}===> $$(fish -v)${RESET}"
 	
 install-fisher: ## Install fisher (https://github.com/jorgebucaran/fisher)
