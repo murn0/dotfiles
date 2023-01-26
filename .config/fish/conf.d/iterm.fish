2719ca641aae9c760a2b15b37e24fb38b243b30f
murn0 <xx.naoyamurano.xx@gmail.com>
2023-01-27 01:04:53 +0900

    fix(iTerm): starship使用時にiTerm2のMark機能が使用できなくなっていたバグに対応
    starship使用時にiTerm2のシェル統合機能の1つであるMarkをプロンプトに追加してショートカットで行き来できるようにする機能がMarkが表示できず使用できなくなっていたバグに対応

--- .config/fish/conf.d/iterm.fish
+++ .config/fish/conf.d/iterm.fish
@@ -1,6 +1,9 @@
 # tmux使用時にもSHELL統合を使用出来る様に設定
 set -xg ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX YES
 
+# starship使用中にプロンプトにMarkを表示させる様に明示的に設定
+set -xg ITERM2_SQUELCH_MARK 1
+
 # SHELL統合ファイルの読み込み
 set ITERM_SHELL_INTEGRATION $XDG_CONFIG_HOME/iterm2/.iterm2_shell_integration.fish
 if test -f $ITERM_SHELL_INTEGRATION
