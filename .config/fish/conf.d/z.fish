0cc22b7db379387e0e263a1a2b865b4ca0b9218d
murn0 <murano@mur.tokyo>
2023-04-21 09:08:56 +0000

    chore: zoxideとfzfを使用した場合の候補の見た目を揃えるために変更
    zoxideでziコマンドを使用した場合に表示されるfzfの見た目が他の場合と異なっていたので、見た目を揃えるために変更

--- .config/fish/conf.d/z.fish
+++ .config/fish/conf.d/z.fish
@@ -1,5 +1,15 @@
 # fzfのオプション設定
-set -Ux _ZO_FZF_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*" --preview=\'exa -la {2..}\''
+set -Ux _ZO_FZF_OPTS '
+    --cycle 
+    --layout=reverse 
+    --border 
+    --height=90% 
+    --preview-window=wrap 
+    --marker="*" 
+    --color=border:#605f5f 
+    --color=fg+:#ffaf00,bg+:#434C5E 
+    --preview=\'exa -la {2..}\'
+'
 
 if command -v zoxide &>/dev/null
     zoxide init fish | source
