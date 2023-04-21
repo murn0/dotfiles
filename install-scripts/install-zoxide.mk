a3a66c4870b6ddd7b0f2e032915803deb67f0eeb
murn0 <murano@mur.tokyo>
2023-04-21 09:22:17 +0000

    refactor: aquaで管理するように変更したツールのインストールスクリプトを削除

--- install-scripts/install-zoxide.mk
+++
@@ -1,9 +0,0 @@
-include $(PWD)/bin/colors.mk
-
-.PHONY: $(shell cat $(MAKEFILE_LIST) | awk -F':' '/^[a-z0-9_-]+:/ {print $$1}')
-
-all: install-zoxide
-
-install-zoxide: ## Install zoxide (https://github.com/ajeetdsouza/zoxide)
-	@curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-	@echo "${BLUE}${BOLD}===> $$(zoxide --version)${RESET}"
