ad70c1cafecbef838959ec604b0a8738f26a41b6
murn0 <xx.naoyamurano.xx@gmail.com>
2023-08-01 17:16:44 +0900

    add wezterm related file path

--- install-scripts/unlinks.mk
+++ install-scripts/unlinks.mk
@@ -5,6 +5,7 @@ XDG_CONFIG_HOME_PATH := $(HOME)/.config
 
 FISH_SETTING_DIR   := $(XDG_CONFIG_HOME_PATH)/fish/conf.d
 FISH_FUNCTION_DIR   := $(XDG_CONFIG_HOME_PATH)/fish/functions
+FISH_MODULES_DIR	:= $(XDG_CONFIG_HOME_PATH)/fish/modules
 ZSH_SETTING_DIR    := $(XDG_CONFIG_HOME_PATH)/zsh/settings
 
 .PHONY: $(shell cat $(MAKEFILE_LIST) | awk -F':' '/^[a-z0-9_-]+:/ {print $$1}')
@@ -15,6 +16,7 @@ unlink-fish-symlinks: ## Unlink symbolic link for fish settings
 	@echo "${RED}▓▒░ Unlink symbolic link for Fish${RESET}"
 	@find $(FISH_SETTING_DIR) -name "*.fish" -type l -exec unlink {} \;
 	@find $(FISH_FUNCTION_DIR) -name "*.fish" -type l -exec unlink {} \;
+	@find $(FISH_MODULES_DIR) -name "*.fish" -type l -exec unlink {} \;
 	@unlink $(XDG_CONFIG_HOME_PATH)/fish/config.fish
 	@unlink $(XDG_CONFIG_HOME_PATH)/fish/fish_plugins
 
