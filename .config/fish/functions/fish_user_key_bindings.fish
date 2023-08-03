f3718663c4dd0523c614f011a23cf749267d7796
murn0 <47878863+murn0@users.noreply.github.com>
2023-08-03 17:34:54 +0900

    feat: add tmux settings (#95)
    * add tmux settings
    
    * change fish key-bindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,9 +1,12 @@
 function fish_user_key_bindings
 
-  # Ctrl+c : Zのディレクトリ移動履歴をfzfで表示
-  bind \cc zi
+  # Ctrl+z : Zのディレクトリ移動履歴をfzfで表示
+  bind \cz zi
   # fzf.fishのキーバインド
-  # Ctrl+r : コマンド履歴検索, Ctrl+s : ファイル、ディレクトリパス検索, Ctrl+v : 変数検索, Ctrl+g : git log検索
-  fzf_configure_bindings --history=\cr --directory=\cs --variables=\cv --git_log=\cg --git_status --processes
+  # Ctrl+r : コマンド履歴検索, Ctrl+f : ファイル、ディレクトリパス検索, Ctrl+v : 変数検索, Ctrl+l : git log検索
+  fzf_configure_bindings --history=\cr --directory=\cf --variables=\cv --git_log=\cl --git_status --processes
+
+  # Ctrl+g : gituiを起動
+  bind \cg gitui
   
 end
