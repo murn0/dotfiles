31a19b33252a234c847da7927ce855fb7d5e91ec
murn0 <xx.naoyamurano.xx@gmail.com>
2023-08-11 15:55:18 +0900

    refactor: fish test command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,9 +4,9 @@ set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
 
 # Add aqua to the environment variable PATH.
-if test -n "$AQUA_ROOT_DIR"
+if [ -n "$AQUA_ROOT_DIR" ]
     fish_add_path -p "$AQUA_ROOT_DIR/bin"
-else if test -n "$XDG_DATA_HOME"
+else if [ -n "$XDG_DATA_HOME" ]
     fish_add_path -p "$XDG_DATA_HOME/aquaproj-aqua/bin"
 else
     fish_add_path -p "$HOME/.local/share/aquaproj-aqua/bin"
@@ -16,29 +16,29 @@ end
 switch (uname)
     case Darwin
         set LOCAL_CONFIG_MACOS $XDG_CONFIG_HOME/fish/modules/config-macos.fish
-        if test -e $LOCAL_CONFIG_MACOS
-            source $LOCAL_CONFIG_MACOS
+        if [ -e "$LOCAL_CONFIG_MACOS" ]
+            source "$LOCAL_CONFIG_MACOS"
         end
     case Linux
         set LOCAL_CONFIG_LINUX $XDG_CONFIG_HOME/fish/modules/config-linux.fish
-        if test -e $LOCAL_CONFIG_LINUX
-            source $LOCAL_CONFIG_LINUX
+        if [ -e "$LOCAL_CONFIG_LINUX" ]
+            source "$LOCAL_CONFIG_LINUX"
         end
     case '*'
         set LOCAL_CONFIG_WINDOWS $XDG_CONFIG_HOME/fish/modules/config-windows.fish
-        if test -e $LOCAL_CONFIG_WINDOWS
-            source $LOCAL_CONFIG_WINDOWS
+        if [ -e "$LOCAL_CONFIG_WINDOWS" ]
+            source "$LOCAL_CONFIG_WINDOWS"
         end
 end
 
 # Define local configuration file
 set LOCAL_CONFIG $XDG_CONFIG_HOME/fish/config-local.fish
-if test -e $LOCAL_CONFIG
-    source $LOCAL_CONFIG
+if [ -e "$LOCAL_CONFIG" ]
+    source "$LOCAL_CONFIG"
 end
 
 # Loading the Shell Integration File for Wezterm
 set WEZTERM_SHELL_INTEGRATION $XDG_CONFIG_HOME/fish/modules/wezterm.fish
-if test -f $WEZTERM_SHELL_INTEGRATION
-    source $WEZTERM_SHELL_INTEGRATION
+if [ -e "$WEZTERM_SHELL_INTEGRATION" ]
+    source "$WEZTERM_SHELL_INTEGRATION"
 end
