8ee621e290abfcc6df14b15c141ad92b171b413b
murn0 <xx.naoyamurano.xx@gmail.com>
2023-08-22 17:29:37 +0900

    Change git log key bindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,7 +3,7 @@ function fish_user_key_bindings
   # Ctrl+z : Zのディレクトリ移動履歴をfzfで表示
   bind \cz zi
   # fzf.fishのキーバインド
-  # Ctrl+r : コマンド履歴検索, Ctrl+f : ファイル、ディレクトリパス検索, Ctrl+v : 変数検索, Ctrl+l : git log検索
-  fzf_configure_bindings --history=\cr --directory=\cf --variables=\cv --git_log=\cl --git_status --processes
+  # Ctrl+r : コマンド履歴検索, Ctrl+f : ファイル、ディレクトリパス検索, Ctrl+v : 変数検索, Ctrl+g : git log検索
+  fzf_configure_bindings --history=\cr --directory=\cf --variables=\cv --git_log=\cg --git_status --processes
 
 end
